2545208da8a92d896c8edfb03d371af1cb59e1a1
Borja <bdesaavazquez@danielcastelao.org>
2025-09-30 09:43:06 +0200

    Tarea8 finalizada

---
+++ src/Tarea8/Tarea8.java
@@ -0,0 +1,44 @@
+package Tarea8;
+
+public class Tarea8 extends Thread {
+
+    private int nivelCabreo;
+
+    /**
+     * Constructor que recibe el nombre del hilo y lo pasa
+     * a la clase padre (Thread) para su inicialización.
+     * @param nombreProfesor El nombre para este hilo.
+     */
+    public Tarea8(String nombreProfesor, int nivelCabreo) {
+        super(nombreProfesor);
+        this.nivelCabreo = nivelCabreo;
+    }
+
+    // Definimos lo que hace el hilo
+    @Override
+    public void run() {
+        for (int i = 0; i <= nivelCabreo; i++) {
+            if(nivelCabreo == i) {
+                System.out.println("[" + getName() + "]" + " Cabreo nivel: " + nivelCabreo + " ...¡He llegado a mi limite!");
+
+            }else {
+                System.out.println("[" + getName() + "]" + " Cabreo nivel: " + i);
+
+            }
+        }
+    }
+
+    /**
+     * El método main es el punto de entrada. Crea y lanza dos
+     * hilos ("Lucas" y "Andy") y luego imprime un mensaje
+     * para indicar que el hilo principal ha terminado sus tareas.
+     */
+    public static void main(String[] args) {
+        new Tarea8("Diego",3).start();
+        new Tarea8("Manuel",3).start();
+        new Tarea8("Damian",4).start();
+        new Tarea8("Araujo",2).start();
+
+        System.out.println("Programa principal terminado");
+    }
+}
